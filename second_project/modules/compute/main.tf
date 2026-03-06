bcefb24964751938bd79b4ff782785fbc705cc22
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-03-06 17:07:31 +0100

    aa

--- second_project/modules/compute/main.tf
+++ second_project/modules/compute/main.tf
@@ -2,8 +2,8 @@ variable "db_endpoint" { type = string }
 variable "subnet_id"   { type = string }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0faab6414a6e30064" # Amazon Linux 2023 in eu-central-1
-  instance_type = "t2.micro"             # Free Tier
+  ami           = "ami-0c42fad2ea005202d"
+  instance_type = "t3.micro"             # Free Tier
   subnet_id     = var.subnet_id
 
   user_data = <<-EOF
