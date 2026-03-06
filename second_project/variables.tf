7fda1f9c4eb3462ea123d50bcde973cce987909b
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-03-06 16:55:27 +0100

    aa

--- second_project/variables.tf
+++ second_project/variables.tf
@@ -1,29 +1,18 @@
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
 }
 
-# RDS richiede subnet in almeno due zone diverse
-resource "aws_subnet" "subnet_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
+variable "aws_access_key" {
+  type = string
 }
 
-resource "aws_subnet" "subnet_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-central-1b"
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
 }
 
-# Creazione del DB Subnet Group obbligatorio per RDS
-resource "aws_db_subnet_group" "main" {
-  name       = "main-db-subnet-group"
-  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
-
-  tags = { Name = "Main DB Subnet Group" }
+variable "db_password" {
+  type      = string
+  sensitive = true
 }
-
-output "public_subnet" { value = aws_subnet.subnet_a.id }
-output "private_subnets" { value = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id] }
-output "db_subnet_group_name" { value = aws_db_subnet_group.main.name }
