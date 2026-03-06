00417acbd01b292c3635a8c38b384a9aa60d9b94
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-03-06 16:19:51 +0100

    aa

--- second_project/modules/database/main.tf
+++ second_project/modules/database/main.tf
@@ -1,16 +1,6 @@
 variable "db_password" { type = string }
 variable "subnet_ids"  { type = list(string) }
 
-resource "aws_db_subnet_group" "postgres_group" {
-  name       = "media-backend-db-group"
-  subnet_ids = var.subnet_ids # Qui colleghiamo le tue subnet reali
-
-  tags = {
-    Name = "Postgres DB Subnet Group"
-  }
-}
-
-
 resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   engine               = "postgres"
@@ -20,8 +10,8 @@ resource "aws_db_instance" "postgres" {
   password             = var.db_password
   skip_final_snapshot  = true
   publicly_accessible  = false
-  db_subnet_group_name = aws_db_subnet_group.postgres_group.name
-  depends_on = [aws_db_subnet_group.postgres_group]
+  subnet_id     = var.subnet_id
+
 }
 
 output "rds_endpoint" {
